aeed1f714e176c2d3ce4aa3f7f1450f48aa84af1
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-12 20:25:22 -0800

    Add automated github stars section for each project panel

--- javascript/app.js
+++ javascript/app.js
@@ -2,22 +2,20 @@ $(document).ready(function() {
   const Url = "https://api.github.com/users/jamisoncozart/repos";
   $.get(Url, function(response, status) {
     var repos = response;
-    // console.log(repos[0].stargazers_count);
+
+    // Sort repo array by number of github stars in descending order
     repos = repos.sort((a,b) => parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count));
-    // console.log(repos[0].stargazers_count);
-    // console.log(repos);
-    // document.getElementById("title").innerHTML = repos[0].name;
-    // document.getElementById("stars").innerHTML = repos[0].stargazers_count;
-    // document.getElementById("description").innerHTML = repos[0].description;
     
     //for each of the top 6 repos, change inner HTML to reflect Github data for title and descriptions.
     var titleIdList = ["repo1Title", "repo2Title", "repo3Title", "repo4Title", "repo5Title", "repo6Title"];
     var descriptionIdList = ["repo1Description", "repo2Description", "repo3Description", "repo4Description", "repo5Description", "repo6Description"];
     var linkIdList = ["repo1link", "repo2link", "repo3link", "repo4link", "repo5link", "repo6link"];
+    var starIdList = ["repo1Stars", "repo2Stars", "repo3Stars", "repo4Stars", "repo5Stars", "repo6Stars"];
     for(let i = 0; i < 6; i++) {
       document.getElementById(titleIdList[i]).innerHTML = repos[i].name;
       document.getElementById(descriptionIdList[i]).innerHTML = repos[i].description;
       document.getElementById(linkIdList[i]).href = repos[i].html_url;
+      document.getElementById(starIdList[i]).innerHTML = repos[i].stargazers_count;
     }
 
     //For each repo, make GET request to repos[0].languages_url to retrieve all languages used.
@@ -27,10 +25,6 @@ $(document).ready(function() {
         var langObj = response;
         var languageNames = Object.keys(langObj);
         var innerHTML = "";
-
-        // var titleIdList = ["repo1Title", "repo2Title", "repo3Title", "repo4Title", "repo1Title", "repo1Title"];
-        // var descriptionIdList = ["repo1Description", "repo1Description", "repo1Description", "repo1Description", "repo1Description", "repo1Description"]
-        
         var ulIdList = ["repo1Languages", "repo2Languages", "repo3Languages", "repo4Languages", "repo5Languages", "repo6Languages"];
         languageNames.forEach(function(language) {
           innerHTML += `<li>${language}</li>`;
