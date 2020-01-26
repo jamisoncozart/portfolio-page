6dd0cf1730d64e624eb29615ff77ac5494bbf3a3
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-26 09:46:32 -0800

    Refactor repo looping and HTML data inserting

--- javascript/app.js
+++ javascript/app.js
@@ -1,27 +1,30 @@
 $(document).ready(function() {
   const Url = "https://api.github.com/users/jamisoncozart/repos?per_page=100";
-  $.get(Url, function(response, status) {
+  $.get(Url, function(response) {
     var repos = response;
 
     // Sort repo array by number of github stars in descending order
-    repos = repos.sort((a,b) => parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count));
+    sortedRepos = repos.sort((a,b) => parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count));
     
     //for each of the top 6 repos, change inner HTML to reflect Github data for title and descriptions.
-    var titleIdList = ["repo1Title", "repo2Title", "repo3Title", "repo4Title", "repo5Title", "repo6Title"];
-    var descriptionIdList = ["repo1Description", "repo2Description", "repo3Description", "repo4Description", "repo5Description", "repo6Description"];
-    var linkIdList = ["repo1link", "repo2link", "repo3link", "repo4link", "repo5link", "repo6link"];
-    var starIdList = ["repo1Stars", "repo2Stars", "repo3Stars", "repo4Stars", "repo5Stars", "repo6Stars"];
-    for(let i = 0; i < 6; i++) {
-      document.getElementById(titleIdList[i]).innerHTML = repos[i].name;
-      document.getElementById(descriptionIdList[i]).innerHTML = repos[i].description;
-      document.getElementById(linkIdList[i]).href = repos[i].html_url;
-      document.getElementById(starIdList[i]).innerHTML = repos[i].stargazers_count;
-    }
+
+    var idList = ["Title", "Description", "link", "Stars"];
+    var objectKeyList = ["name", "description", "html_url", "stargazers_count"];
+    idList.forEach(function(id, index) {
+      for(let i = 1; i <= 6; i++) {
+        var currentID = "repo" + i + id;
+        if(id === "link") {
+          document.getElementById(currentID).href = sortedRepos[i][objectKeyList[index]];
+        } else {
+          document.getElementById(currentID).innerHTML = sortedRepos[i][objectKeyList[index]];
+        }
+      }
+    })
 
     //For each repo, make GET request to repos[0].languages_url to retrieve all languages used.
     //Generate new <li></li> for each language in the response "languages" object
     for(let i = 0; i < 6; i++) {
-      $.get(repos[i].languages_url, function(response, status) {
+      $.get(repos[i].languages_url, function(response) {
         var langObj = response;
         var languageNames = Object.keys(langObj);
         var innerHTML = "";
